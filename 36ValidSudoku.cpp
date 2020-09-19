28b1329255bfd30296643b134192217c74d91ec6
Yash Chaudhari <chaudhariyash10@gmail.com>
2020-09-19 10:04:09 +0530

    Valid Sudoku

---
+++ 36ValidSudoku.cpp
@@ -0,0 +1,92 @@
+#include <stdc++.h>
+
+using namespace std;
+
+bool isRowValid(vector<vector<char>> &board, int r)
+{
+    unordered_map<char, bool> umap;
+
+    for (int i = 0; i < 9; i++)
+    {
+        if (board[r][i] == '.')
+            continue;
+        if (umap[board[r][i]])
+        {
+            return false;
+        }
+        else
+        {
+            umap[board[r][i]] = true;
+        }
+    }
+
+    return true;
+}
+bool isColumnValid(vector<vector<char>> &board, int c)
+{
+    unordered_map<char, bool> umap;
+
+    for (int i = 0; i < 9; i++)
+    {
+        if (board[i][c] == '.')
+            continue;
+
+        if (umap[board[i][c]])
+        {
+            return false;
+        }
+        else
+        {
+            umap[board[i][c]] = true;
+        }
+    }
+
+    return true;
+}
+
+bool isSquareValid(vector<vector<char>> &board, int r, int c)
+{
+    unordered_map<char, bool> umap;
+
+    for (int i = r - 1; i < r + 2; i++)
+    {
+        for (int j = c - 1; j < c + 2; j++)
+        {
+            if (board[i][j] == '.')
+                continue;
+
+            if (umap[board[i][j]])
+            {
+                return false;
+            }
+            else
+            {
+                umap[board[i][j]] = true;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool isValidSudoku(vector<vector<char>> &board)
+{
+    bool ans = true;
+
+    for (int i = 0; i < 9; i++)
+    {
+        ans = (isRowValid(board, i) && isColumnValid(board, i));
+
+        if (!ans)
+            return ans;
+    }
+
+    for (int i = 1; i < 9; i = i + 3)
+    {
+        ans = (isSquareValid(board, i, 1) && isSquareValid(board, i, 4) && isSquareValid(board, i, 7));
+        if (!ans)
+            return ans;
+    }
+
+    return ans;
+}
