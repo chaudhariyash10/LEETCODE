544777f1cc9f1f3bc6379d25d95039c5485e27c2
Yash Chaudhari <chaudhariyash10@gmail.com>
2020-11-13 13:57:28 +0530

    More quetions

--- 50Pow(x,n).cpp
+++ 50Pow(x,n).cpp
@@ -7,9 +7,9 @@ double myPow(double x, long n)
     if (n == 0)
         return 1;
     if (n > 0)
-        return Pow(x, n);
+        return myPow(x, n);
     else
-        return 1 / Pow(x, -n);
+        return 1 / myPow(x, -n);
 }
 double Pow(double x, long n)
 {
@@ -17,13 +17,12 @@ double Pow(double x, long n)
         return 1;
 
     else if (n % 2 == 0)
-        return myPow(x * x, n / 2);
+        return Pow(x * x, n / 2);
 
     else
-        return x * myPow(x * x, (n - 1) / 2);
+        return x *
+               Pow(x * x, (n - 1) / 2);
 }
-
-
 
 int main()
 {
@@ -36,5 +35,5 @@ int main()
     cout << "Enter n: ";
     cin >> n;
 
-    cout << Pow(x, n);
+    cout << Pow(x, n) << endl;
 }
