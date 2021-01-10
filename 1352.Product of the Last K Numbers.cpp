fb41c83f4fd5b4dd6bc04d9d14ce13f15926fedf
Yash Chaudhari <chaudhariyash10@gmail.com>
2021-01-10 15:08:04 +0530

    maximum subarray product

---
+++ 1352.Product of the Last K Numbers.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class ProductOfNumbers
+{
+    
+public:
+    ProductOfNumbers()
+    {
+    }
+
+    void add(int num)
+    {
+    }
+
+    int getProduct(int k)
+    {
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
--- 152MaximumSubarrayProduct.cpp
+++ 152MaximumSubarrayProduct.cpp
@@ -7,15 +7,20 @@ class Solution
 public:
     int maxProduct(vector<int> &nums)
     {
-        int maxPro = INT_MAX, curPro = 1;
+        if(nums.size() == 1)
+            return nums[0];
+        int maxPro = nums[0], curPro = nums[0], minPro = nums[0], curMin = nums[0];
 
-        for (int i = 0; i < nums.size(); i++)
+        for(int i = 1; i < nums.size(); i++)
         {
-            for (int j = i; j < nums.size(); j++)
-            { 
-                
-            }
-        }
+            int tmp = nums[i] * curPro;
+            curPro = max(curPro * nums[i], nums[i]);
+            curPro = max(curPro, minPro * nums[i]);
+            maxPro = max(maxPro, curPro);
+
+            minPro = min(nums[i], minPro * nums[i]);
+            minPro = min(tmp, minPro );
+        }   
         return maxPro;
     }
 };
