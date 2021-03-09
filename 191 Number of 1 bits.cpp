19cf90a387b7a2287f7aac2afb9f18d698446137
Yash Chaudhari <chaudhariyash10@gmail.com>
2021-03-09 14:24:33 +0530

    Number of 1 bits

---
+++ 191 Number of 1 bits.cpp
@@ -0,0 +1,18 @@
+#include <stdc++.h>
+
+using namespace std;
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int count = 0;
+
+        while (n > 0)
+        {
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+    }
+};
